746d594baf9bb3b999ca992486fc115992dde88f
Drop <add7348@outlook.com>
2024-11-09 23:32:22 +0800

    finish program for spl-token-minter

--- programs/fork/spl-token-minter/src/lib.rs
+++ programs/fork/spl-token-minter/src/lib.rs
@@ -1,13 +1,7 @@
-pub mod constants;
-pub mod error;
 pub mod instructions;
-pub mod state;
 
 use anchor_lang::prelude::*;
-
-pub use constants::*;
-pub use instructions::*;
-pub use state::*;
+use instructions::*;
 
 declare_id!("D8CuAy6k5kFtTVXhkT2kZadUz6b49Hd9voeRiFcPzbM1");
 
@@ -15,4 +9,16 @@ declare_id!("D8CuAy6k5kFtTVXhkT2kZadUz6b49Hd9voeRiFcPzbM1");
 pub mod fork_spl_token_minter {
     use super::*;
 
+    pub fn create_token(
+        ctx: Context<CreateToken>,
+        token_name: String,
+        token_symbol: String,
+        token_uri: String,
+    ) -> Result<()> {
+        create::create_token(ctx, token_name, token_symbol, token_uri)
+    }
+
+    pub fn mint_token(ctx: Context<MintToken>, amount: u64) -> Result<()> {
+        mint::mint_token(ctx, amount)
+    }
 }
